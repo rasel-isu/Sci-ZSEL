e353cce287f1ca441b6ac99a975a8fc86fb5dc24
rasel-isu <rasel@iastate.edu>
2026-09-04 00:21:00 -0500

    readme

--- modeling/ReS/res_reranker_fine_tuning.sh
+++ modeling/ReS/res_reranker_fine_tuning.sh
@@ -2,6 +2,6 @@ eval $(/lustre/hdd/LAS/qli-lab/rasel/apps/miniconda3/bin/conda shell.bash hook)
 source /lustre/hdd/LAS/qli-lab/rasel/apps/miniconda3/etc/profile.d/conda.sh
 conda activate sci-zsel-res
 export WANDB_MODE=disabled
-# python get_retriever_candidates.py
+python get_retriever_candidates.py
 python train_and_eval.py 
 
